ed75a7034e4a683178ca9f2f182624b516f31330
Vitor Lourenco <vitor.registros@lourencos.net>
2021-07-22 13:28:01 -0300

    Handle database conflict exception for method post of /genres

--- src/services/genreService.ts
+++ src/services/genreService.ts
@@ -2,7 +2,7 @@ import genreRepository from "../repositories/genreRepository";
 
 async function create(body:{name:string}){
   const dbNewGenre = await genreRepository.create(body.name);
-  return dbNewGenre.rows[0];
+  return dbNewGenre?.rows[0];
 }
 
 export default {
