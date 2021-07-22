a0073c4d865fcf8d8ad5cf6f5a5289eab375f970
Vitor Lourenco <vitor.registros@lourencos.net>
2021-07-22 15:59:28 -0300

    Refactor tests for route /recommendations method post

--- tests/integration/recommendations.test.ts
+++ tests/integration/recommendations.test.ts
@@ -3,11 +3,24 @@ import "../../src/setup";
 import supertest from "supertest";
 import app from "../../src/app";
 import toMatchSchema from "../schemas/toMatchSchema";
+import recommendations from '../utils/recommendations';
 import recommendationSchemas from "../schemas/recommendationSchemas";
-import { clearDatabase, closeConnection } from "../utils/database";
+import { clearDatabase, closeConnection , clearRecommendations} from "../utils/database";
 import { createGenre } from "../factories/genreFactory";
 
 expect.extend({ toMatchSchema });
+
+beforeAll(async()=>{
+  await clearDatabase();
+  //recommendations.valid needs valid genres
+  //create 2 valid genres with ids 1 and 2
+  await createGenre();
+  await createGenre();
+})
+
+beforeEach(async()=>{
+  await clearRecommendations();
+})
 
 afterAll(() => {
   closeConnection();
@@ -15,57 +28,45 @@ afterAll(() => {
 
 describe("POST /recommendations", () => {
   beforeEach(async () => {
-    await clearDatabase();
-    await createGenre();
-    await createGenre();
+    await clearRecommendations();
   });
 
   const postThis = async (data: object) =>
     await supertest(app).post("/recommendations").send(data);
 
-  //genreIds id=1 and id=2 are created in the database beforeEach test run
-  //checkout clearDatabase() for sequence reset (can't truncate FK tables)
-  const validRecommendation = {
-    name: "Test recommendation",
-    genreIds: [1, 2],
-    youtubeLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-  };
-  const wrongTypeRecommendation: any[] = [];
-  const missingPropsRecommendation = {};
-  const emptyPropsRecommendation: {
-    name: string;
-    genreIds: any[];
-    youtubeLink: string;
-  } = { name: "", genreIds: [], youtubeLink: "" };
-
   it("should respond with status 201 for a successful request", async () => {
-    const response = await postThis(validRecommendation);
+    const response = await postThis(recommendations.valid);
     expect(response.status).toEqual(201);
   });
 
-  it("should respond with the valid created genre", async () => {
-    const response = await postThis(validRecommendation);
+  it("should respond with the valid created recommendation", async () => {
+    const response = await postThis(recommendations.valid);
     expect(response.body).toMatchSchema(recommendationSchemas.dbRecommendation);
   });
 
   it("should respond with status 400 for a payload that is not an Object", async () => {
-    const response = await postThis(wrongTypeRecommendation);
+    const response = await postThis(recommendations.wrongType);
     expect(response.status).toEqual(400);
   });
 
   it("should respond with status 400 when the properties are missing", async () => {
-    const response = await postThis(missingPropsRecommendation);
+    const response = await postThis(recommendations.missingProps);
     expect(response.status).toEqual(400);
   });
 
   it("should respond with status 400 when the properties are empty", async () => {
-    const response = await postThis(emptyPropsRecommendation);
+    const response = await postThis(recommendations.emptyProps);
     expect(response.status).toEqual(400);
   });
 
+  it("should respond with status 406 when any genreId does not exist", async()=>{
+    const response = await postThis(recommendations.genreIdNotRegistered);
+    expect(response.status).toEqual(406);
+  })
+
   it("should respond with status 409 when the youtubeLink is taken", async () => {
-    await postThis(validRecommendation);
-    const response = await postThis(validRecommendation);
+    await postThis(recommendations.valid);
+    const response = await postThis(recommendations.valid);
     expect(response.status).toEqual(409);
   });
 });
--- tests/utils/database.ts
+++ tests/utils/database.ts
@@ -1,11 +1,16 @@
 import connection from "../../src/database";
 
 export async function clearDatabase () {
+  await connection.query(`TRUNCATE genres_recommendations RESTART IDENTITY`);
   await connection.query(`DELETE FROM genres`);
   await connection.query(`ALTER SEQUENCE genres_id_seq RESTART WITH 1`);
   await connection.query(`DELETE FROM recommendations`);
   await connection.query(`ALTER SEQUENCE recommendations_id_seq RESTART WITH 1`);
+}
+
+export async function clearRecommendations (){
   await connection.query(`TRUNCATE genres_recommendations RESTART IDENTITY`);
+  await connection.query(`DELETE FROM recommendations`);
 }
 
 export async function closeConnection () {
